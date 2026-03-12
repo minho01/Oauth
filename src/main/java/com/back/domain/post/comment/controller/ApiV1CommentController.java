68793c917a3739dfddb19d91aca9196eb3fb70e3
chacha86 <taejincha0619@gmail.com>
2026-03-12 13:19:24 +0900

    33, 34

--- src/main/java/com/back/domain/post/comment/controller/ApiV1CommentController.java
+++ src/main/java/com/back/domain/post/comment/controller/ApiV1CommentController.java
@@ -68,8 +68,6 @@ public class ApiV1CommentController {
         Post post = postService.findById(postId).get();
         Comment comment = post.addComment(reqBody.content);
 
-        postService.flush();
-
         return new RsData<>(
                 "%d번 댓글이 성공적으로 작성되었습니다.".formatted(comment.getId()),
                 "201-1",
