90f83976b80e4352b85fd6b3bbd3948f83b7112e
minho <minho01@hs.ac.kr>
2026-03-27 12:15:36 +0900

    10

---
+++ src/main/java/com/back/global/exception/ServiceException.java
@@ -0,0 +1,17 @@
+package com.back.global.exception;
+
+import com.back.global.rsData.RsData;
+
+public class ServiceException extends RuntimeException{
+    private String msg;
+    private String resultCode;
+
+    public ServiceException(String resultCode, String msg){
+        super(msg);
+        this.msg = msg;
+    }
+
+    public RsData<Void> getRsData(){
+        return new RsData<>(msg, resultCode);
+    }
+}
