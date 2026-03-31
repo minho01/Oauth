dd7382f936183a3a15d70c3eff447d0438b98d7c
minho <minho01@hs.ac.kr>
2026-03-31 10:33:21 +0900

    44

--- src/test/java/com/back/domain/post/member/controller/ApiV1MemberControllerTest.java
+++ src/test/java/com/back/domain/post/member/controller/ApiV1MemberControllerTest.java
@@ -138,6 +138,9 @@ public class ApiV1MemberControllerTest {
 
                     assertThat(apiKeyCookie).isNotNull();
                     assertThat(apiKeyCookie.getValue()).isEqualTo(apiKey);
+                    assertThat(apiKeyCookie.getPath()).isEqualTo("/");
+                    assertThat(apiKeyCookie.isHttpOnly()).isTrue();
+                    assertThat(apiKeyCookie.getDomain()).isEqualTo("localhost");
 
                     if(apiKeyCookie != null) {
                         assertThat(apiKeyCookie.getValue()).isNotBlank();
