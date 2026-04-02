76272d9434d570b3d832aa747998eb8004e0abaf
minho <minho01@hs.ac.kr>
2026-04-02 16:02:18 +0900

    84

--- src/main/java/com/back/global/security/CustomAuthenticationFilter.java
+++ src/main/java/com/back/global/security/CustomAuthenticationFilter.java
@@ -11,6 +11,11 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.io.IOException;
 import java.util.List;
@@ -92,6 +97,23 @@ public class CustomAuthenticationFilter extends OncePerRequestFilter {
         }
 
         // SecurityContextHolder에 인증데이터 저장
+
+        UserDetails user = new User(
+                member.getUsername(),
+                member.getPassword(),
+                List.of()
+        );
+
+        Authentication authentication = new UsernamePasswordAuthenticationToken(
+                user,
+                null,
+                user.getAuthorities()
+        );
+
+        SecurityContextHolder
+                .getContext()
+                .setAuthentication(authentication);
+
         filterChain.doFilter(request, response);
     }
 
